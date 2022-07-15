c275cc834b6df94d595ca8d102cc307cf83834a2
huangchucai <425205839@qq.com>
2022-07-15 20:58:33 +0800

    feat: 添加二叉树的遍历

---
+++ leetcode/editor/cn/[94]二叉树的中序遍历.ts
@@ -0,0 +1,67 @@
+//给定一个二叉树的根节点 root ，返回 它的 中序 遍历 。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：root = [1,null,2,3]
+//输出：[1,3,2]
+//
+//
+// 示例 2：
+//
+//
+//输入：root = []
+//输出：[]
+//
+//
+// 示例 3：
+//
+//
+//输入：root = [1]
+//输出：[1]
+//
+//
+//
+//
+// 提示：
+//
+//
+// 树中节点数目在范围 [0, 100] 内
+// -100 <= Node.val <= 100
+//
+//
+//
+//
+// 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+// Related Topics 栈 树 深度优先搜索 二叉树 👍 1495 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function inorderTraversal(root: TreeNode | null): number[] {
+  let result: number[]= []
+  function walk(root:  TreeNode | null) {
+    if(!root) return
+    walk(root.left)
+    result.push(root.val)
+    walk(root.right)
+  }
+  walk(root)
+  return result
+};
+//leetcode submit region end(Prohibit modification and deletion)
